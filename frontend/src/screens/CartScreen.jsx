336e387a82b7f145870f9a529a2570aa89af370a
Lerato Moshabi <lerato.moshabi@lexisnexis.co.za>
2023-10-07 19:35:41 +0200

    Added R Currency for Admin Cart

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -90,7 +90,7 @@ const CartScreen = () => {
                 Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
                 items
               </h2>
-              $
+              R
               {cartItems
                 .reduce((acc, item) => acc + item.qty * item.price, 0)
                 .toFixed(2)}
